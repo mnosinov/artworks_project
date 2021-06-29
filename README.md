090321e6fb06f3311d07ed18cc72ae522b56239f
mirbek <mnosinov@gmail.com>
2021-06-29 18:53:21 +0600

    intermediate commit

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ sample django app to research django-formtools lib
 
 # .env file
 SECRET_KEY=ADSFKJKLEFJLAKDSFKEJFKLADKFI123123123123123123____23323
+
+ALLOWED_HOSTS_LIST=localhost, 127.0.0.1
+
+DEBUG_MODE=True
--- artworks/models.py
+++ artworks/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django_resized import ResizedImageField
 
-# Create your models here.
+from django.utils.translation import gettext as _
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class ArtworkType(models.TextChoices):
+    PAINTING = 'PAINTING', _('Painting')
+    MUSIC = 'MUSIC', _('Music')
+    MOVIE = 'MOVIE', _('Movie')
+    BOOK = 'BOOK', _('BooK')
+
+
+class Genre(models.Model):
+    artwork_type = models.CharField(max_length=30, choices=ArtworkType.choices,
+                                    null=False, blank=False)
+    title = models.CharField(max_length=200, null=False, blank=False)
+
+
+class Artwork(models.Model):
+    type = models.CharField(max_length=30, choices=ArtworkType.choices, null=False, blank=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200, null=False, blank=False)
+    pub_year = models.IntegerField(null=True, blank=True)
+
+
+class Painting(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
+    # TODO TODO 

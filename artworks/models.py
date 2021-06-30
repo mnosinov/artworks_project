f8e88d85b78b47773b6960827b9b3b7452e8f656
werty <mnosinov@gmail.com>
2021-06-30 10:15:28 +0600

    models are ready to use

--- artworks/models.py
+++ artworks/models.py
@@ -16,18 +16,38 @@ class ArtworkType(models.TextChoices):
 
 
 class Genre(models.Model):
-    artwork_type = models.CharField(max_length=30, choices=ArtworkType.choices,
-                                    null=False, blank=False)
-    title = models.CharField(max_length=200, null=False, blank=False)
+    artwork_type = models.CharField(max_length=30, choices=ArtworkType.choices)
+    title = models.CharField(max_length=200)
 
 
 class Artwork(models.Model):
-    type = models.CharField(max_length=30, choices=ArtworkType.choices, null=False, blank=False)
+    # type is mandatory field
+    type = models.CharField(max_length=30, choices=ArtworkType.choices)
+    # genre can be unknown - null
+    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=200, null=False, blank=False)
+    title = models.CharField(max_length=200)
     pub_year = models.IntegerField(null=True, blank=True)
+    price = models.IntegerField(null=True, blank=True)
 
 
 class Painting(models.Model):
-    genre = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True, blank=True)
-    # TODO TODO 
+    artwork = models.OneToOneField(Artwork, on_delete=models.CASCADE, primary_key=True)
+    height = models.IntegerField()
+    width = models.IntegerField()
+    paint = models.CharField(max_length=30)
+
+
+class Book(models.Model):
+    artwork = models.OneToOneField(Artwork, on_delete=models.CASCADE, primary_key=True)
+    pages = models.IntegerField()
+    cover = ResizedImageField(upload_to='books/covers/%Y/%m/%d')
+
+
+class Media(models.Model):
+    class MediaType(models.TextChoices):
+        MUSIC = 'MUSIC', _('Music')
+        VIDEO = 'VIDEO', _('Video')
+    artwork = models.OneToOneField(Artwork, on_delete=models.CASCADE, primary_key=True)
+    media_type = models.CharField(max_length=10, choices=MediaType.choices)
+    duration = models.DurationField()
